9fbd2756a58e415cef5dbb6d8a00352da75e939c
bluecaterpillar <diego.carrea87@gmail.com>
2024-04-26 14:35:31 +0200

    DB Setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  MY_APP_DATABASE_URL = "https://fly.io/apps/five-ingredients-app-db"
 end
